75aa0e5c6bae4f99eed52cff62b9d56860ebba7d
gsksivesh <gsksivesh@gmail.com>
2016-02-26 23:32:27 +0530

    Update sortedArraysCommonElements.cpp

--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -21,7 +21,60 @@ struct transaction {
 	char date[11];
 	char description[20];
 };
-
-struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+int date_to_number4(char *date)
+{
+	int i;
+	int idate = 0, imonth = 0, iyear = 0;
+	for (i = 0; date[i] != '\0'; i++)
+	{
+		if (date[i] > '9' || date[i] < '0')
+			break;
+		idate = idate * 10 + (date[i] - '0');
+	}
+	i++;
+	for (; date[i] != '\0'; i++)
+	{
+		if (date[i] > '9' || date[i] < '0')
+			break;
+		imonth = imonth * 10 + (date[i] - '0');
+	}
+	i++;
+	for (; date[i] != '\0'; i++)
+	{
+		if (date[i] > '9' || date[i] < '0')
+			break;
+		iyear = iyear * 10 + (date[i] - '0');
+	}
+	idate = idate + imonth * 100 + iyear * 10000;
+	return idate;
+}
+struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) 
+{
+	struct transaction *ans;
+	int Aindex, Bindex;
+	int ansindex=0;
+	int Adate, Bdate;
+	if (A == NULL || B == NULL)
+		return NULL; 
+	ans = (struct transaction*)malloc(sizeof(struct transaction));
+	
+	for (Aindex = 0; Aindex < ALen; Aindex++)
+	{
+		Adate = date_to_number4(A[Aindex].date);
+		for (Bindex = 0; Bindex < BLen; Bindex++)
+		{
+			Bdate = date_to_number4(B[Bindex].date);
+			if (Adate < Bdate)
+				break;
+			else if (Adate == Bdate)
+			{
+				ans[ansindex] = B[Bindex];
+				ansindex++;
+				ans = (struct transaction*)realloc(ans, sizeof(struct transaction)*(ansindex + 1));
+			}
+		}
+	}
+	if (ansindex == 0)
+		return NULL;
+	return ans;
 }
